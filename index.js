e1affd2d848d9028a5f78fb6a70b4045972aabc4
macbook <macbook@macbook.local>
2024-02-10 10:57:02 +0530

    chore regular

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const wishListRoute = require("./routes/wishlist");
 const paymentRouter = require("./routes/payment");
 const { connectDB } = require("./config/dbconnection");
 
+
 const cors = require("cors");
 const server = express();
 
