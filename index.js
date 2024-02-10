39f853595440151398020cf09f6b4ff97877f141
macbook <macbook@macbook.local>
2024-02-10 10:28:31 +0530

    fix: merge conflicts

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
